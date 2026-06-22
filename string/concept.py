86db5bf6d3d4c73b3c0f73bec6c2af317e273bce
sharmaJi <prakashdeep2902@gmail.com>
2026-06-22 16:29:09 +0530

    added

--- string/concept.py
+++ string/concept.py
@@ -1,22 +1,25 @@
-s = """Twinkle,twinkle, 
-little star,How I wonder what you are!Up above the world so high
-,Like a diamond in the sky.Twinkle,= twinkle, little star,How I wonder what you are! """
+# s = """Twinkle,twinkle,
+# little star,How I wonder what you are!Up above the world so high
+# ,Like a diamond in the sky.Twinkle,= twinkle, little star,How I wonder what you are! """
 
 
-trimedString = s.strip()
+# trimedString = s.strip()
 
-# print(s.split("="))
+# # print(s.split("="))
 
 
-# print(len(s.strip()))
-# print(s[10:])
+# # print(len(s.strip()))
+# # print(s[10:])
 
-print(trimedString)
+# print(trimedString)
 
-print(trimedString[-20:-1])
+# print(trimedString[-20:-1])
 
-s2 = "prakash is good boy!"
+# s2 = "prakash is good boy!"
 
-print(s2[-1:-10])
+# print(s2[-1:-10])
 
 # slicing of string
+
+word = "amazing"
+print(word[1:7:2])
